dac5f02a19864fc87fed237ad1426d5b77182bb3
djlove254 <108491587+djlove254@users.noreply.github.com>
2026-08-28 11:52:32 +0300

    Update SettingsSeeder.php

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -5,26 +5,31 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Setting;
 
-class SettingsSeeder extends Seeder {
-    public function run(): void {
+class SettingsSeeder extends Seeder
+{
+    public function run(): void
+    {
         $settings = [
-            ['key' => 'school_name',    'value' => 'Al-Noor Public School'],
-            ['key' => 'school_email',   'value' => 'info@school.com'],
-            ['key' => 'school_phone',   'value' => '+92-300-0000000'],
-            ['key' => 'school_address', 'value' => 'Hyderabad, Sindh, Pakistan'],
+            ['key' => 'school_name',    'value' => 'CBC School Management System'],
+            ['key' => 'principal_name', 'value' => 'School Principal'],
+            ['key' => 'school_email',   'value' => 'info@school.ac.ke'],
+            ['key' => 'school_phone',   'value' => '+254700000000'],
+            ['key' => 'school_address', 'value' => 'Kenya'],
             ['key' => 'school_logo',    'value' => 'logo.png'],
-            ['key' => 'currency',       'value' => 'PKR'],
-            ['key' => 'timezone',       'value' => 'Asia/Karachi'],
-            ['key' => 'session_year',   'value' => '2025-2026'],
-            ['key' => 'school_tagline', 'value' => 'Education is the key to success'],
-            ['key' => 'fine_per_day',   'value' => '5'],
+            ['key' => 'currency',       'value' => 'KES'],
+            ['key' => 'timezone',       'value' => 'Africa/Nairobi'],
+            ['key' => 'session_year',   'value' => '2026'],
+            ['key' => 'school_tagline', 'value' => 'Empowering Learners Through CBC'],
+            ['key' => 'fine_per_day',   'value' => '50'],
         ];
+
         foreach ($settings as $setting) {
             Setting::firstOrCreate(
                 ['key' => $setting['key']],
                 ['value' => $setting['value']]
             );
         }
-        echo "Settings Created!\n";
+
+        echo "Kenyan school settings created!\n";
     }
 }
